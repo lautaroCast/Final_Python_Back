dddc45a3d5939fabb03a8181f8195f8fc9df84b1
lautaroCast <lauticastillo2016@gmail.com>
2026-02-07 21:25:56 -0300

    elimino recursion producto-categoria.

--- schemas/product_schema.py
+++ schemas/product_schema.py
@@ -19,6 +19,6 @@ class ProductSchema(BaseSchema):
 
     category_id: int = Field(..., description="Category ID reference (required)")
 
-    category: Optional['CategorySchema'] = None
+    """category: Optional['CategorySchema'] = None"""
     reviews: Optional[List['ReviewSchema']] = []
     order_details: Optional[List['OrderDetailSchema']] = []
